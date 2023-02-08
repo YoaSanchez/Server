c074f3cfc3dbdcfc0f31ccbf952c77eb96fd2352
YoaSanchez <yoa192@gmail.com>
2023-02-08 03:18:33 -0300

    ahora si muestra los archivos en /products

--- ProductManager.js
+++ ProductManager.js
@@ -71,18 +71,5 @@ class ProductManager {
 }
 
 const productManager = new ProductManager('products.json');
-const products = productManager.getProducts();
-
-productManager.addProduct("Product 1", "Description 1", 10, "path/to/img", "P001", 20);
-productManager.addProduct("Product 2", "Description 2", 20, "path/to/img", "P002", 30);
-productManager.addProduct("Product 3", "Description 3", 20, "path/to/img", "P003", 17);
-productManager.addProduct("Product 4", "Description 4", 20, "path/to/img", "P004", 9);
-productManager.addProduct("Product 5", "Description 5", 312, "path/to/img", "P005", 34);
-productManager.addProduct("Product 6", "Description 6", 213, "path/to/img", "P006", 13);
-productManager.addProduct("Product 7", "Description 7", 132, "path/to/img", "P007", 7);
-productManager.addProduct("Product 8", "Description 8", 231, "path/to/img", "P008", 24);
-productManager.addProduct("Product 9", "Description 9", 321, "path/to/img", "P009", 10);
-productManager.addProduct("Product 10", "Description 10", 123, "path/to/img", "P0010", 5);
-console.log(products);
 
 module.exports = ProductManager;
--- ServerExpress.js
+++ ServerExpress.js
@@ -1,24 +1,31 @@
-const express = require('express');
-const ProductManager = require('./ProductManager');
+const express = require('express'); // Importamos la librería express
+const ProductManager = require('./ProductManager'); // Importamos la clase ProductManager
 
-const app = express();
-const productManager = new ProductManager();
+const app = express(); // Creamos una instancia de express
+const productManager = new ProductManager('products.json'); // Creamos una instancia de ProductManager
 
+// Define la ruta GET "/products"
 app.get('/products', async (req, res) => {
-  let products = await productManager.getProducts();
-  const limit = req.query.limit;
+  let products = await productManager.getProducts(); // Obtiene todos los productos a través de la clase ProductManager
+  const limit = req.query.limit; // Obtiene el límite opcional desde la consulta
   if (limit) {
-    products = products.slice(0, limit);
+    products = products.slice(0, limit); // Limita la cantidad de productos si el límite fue especificado
   }
-  res.json({ products });
+  res.json({ products }); // Devuelve los productos en formato JSON
 });
 
+// Define la ruta GET "/products/:pid"
 app.get('/products/:pid', async (req, res) => {
-  const pid = req.params.pid;
-  const product = await productManager.getProductById(pid);
-  res.json({ product });
+  const pid = req.params.pid; // Obtiene el ID del producto desde los parámetros de la ruta
+  const product = await productManager.getProductById(pid); // Obtiene el producto específico a través de la clase ProductManager
+  if (product) { //Verificamos que el producto con el pid utilizado exista
+    res.json({ product }); // Devuelve el producto en formato JSON
+  } else { // Si no existe da un error y avisa que no existe
+    res.status(404).json({ error: `Product with ID '${pid}' not found.` });
+  }
 });
 
+// Inicia el servidor en el puerto 8080
 app.listen(8080, () => {
-  console.log('Server running on port 8080');
+  console.log('Server running on port 8080'); // Muestra un mensaje en la consola cuando el servidor se inicia correctamente
 });
